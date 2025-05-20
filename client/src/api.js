b714aa74ed06a3039a9934de7319086e8abc5e0f
Mehmet Efe Servili <99538176+zoeyefe@users.noreply.github.com>
2025-05-20 08:02:41 +0300

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 export async function fetchStatus() {
-  const res = await fetch('http://localhost:5000/api/status');
+  const res = await fetch('https://statusago.onrender.com/api/status');
   if (!res.ok) throw new Error('Durum alınamadı');
   return res.json();
 } 
