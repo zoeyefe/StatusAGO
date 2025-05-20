12825b88e7183df6fe2351bc619452db2b76fcea
Mehmet Efe Servili <eservili42@gmail.com>
2025-05-20 19:44:20 +0300

    refactor: remove deprecated environment variable definition in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
